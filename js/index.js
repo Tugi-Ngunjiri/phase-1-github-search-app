8135262e6a98b71dcc93918fdb912a739781e1ee
tugi <blueqwinn@gmail.com>
2023-04-20 15:53:36 +0300

    add div class

--- js/index.js
+++ js/index.js
@@ -11,8 +11,7 @@ const searchTerm = form.querySelector('input').value;
     console.log(data);
 
     const users = data.map(user=> {
-      let user =document.createElement('div')
-       user.classList.add = ("col -md-3")
+user.classList.add = ("col -md-3")
        user.innerHTML = ` <div>
    <a href="${repo.owner.html_url}">${repo.owner.login}</a>
     </div>`
