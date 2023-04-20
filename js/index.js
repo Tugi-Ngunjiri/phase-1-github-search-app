64f29e7c18c881c98bbdbf470e4c6aa8c10e569e
tugi <blueqwinn@gmail.com>
2023-04-20 15:50:31 +0300

    add user data

--- js/index.js
+++ js/index.js
@@ -1,22 +1,32 @@
 document.addEventListener("DOMContentLoaded", () =>{
-  fetch ('https://api.github.com/users/octocat/repos')
-  .then(response => response.json())
-  .then(data => {
-  
-  })
-  const userContent = "https://api.github.com/users/octocat/repos";
-  console.log(userContent)
-const repos = data.map(repo => {
-     data.users.forEach(user => {
-      let user =document.createElement('div')
-      user.classList.add = ("col -md-3")
-      user.innerHTML = ` <div>
-  <a href="${repo.owner.html_url}">${repo.owner.login}</a>
-   </div>`
-   userContent.appendChild(userName);
-     });
- 
-     })  
- })
+  const form =document.querySelector('form');
+  form.addEventListener('submit', (e) => {
+ e.preventDefault();
 
+const searchTerm = form.querySelector('input').value;
+
+ fetch("https://api.github.com/search/users?q=" +searchTerm)
+  .then(res => res.json())
+  .then( data => {
+    console.log(data);
+
+    const users = data.map(user=> {
+      let user =document.createElement('div')
+       user.classList.add = ("col -md-3")
+       user.innerHTML = ` <div>
+   <a href="${repo.owner.html_url}">${repo.owner.login}</a>
+    </div>`
+    userContent.appendChild(userName);
+      });
+  
+      
+  })
  
+  
+
+  });
+  
+   })
+
+
+
